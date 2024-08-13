01f3b80c750677bb0603889decbc53ce395c5528
Brian <briancmuhammad@yahoo.com>
2024-08-13 10:14:33 -0700

    Added nav bar with repsonsive feature

--- src/compoanents/Nav.jsx
+++ src/compoanents/Nav.jsx
@@ -1,8 +1,29 @@
 import React from "react";
+
+//import fontawesome from '@fortawesome/fontawesome'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import LibraryLogo from "../assets/Library.svg";
 import { Link } from "react-router-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+
+//import { faBars} from '@fortawesome/fontawesome-free-solid'
+
+library.add(faBars, faTimes);
 
 const Nav = () => {
+
+  function openMenu() {
+    document.body.classList += " menu--open";
+  }
+
+  function closeMenu() {
+    document.body.classList.remove("menu--open");
+  }
+ 
+  
   return (
     <nav>
       <div className="nav__container">
@@ -25,7 +46,34 @@ const Nav = () => {
               Contact
             </Link>
           </li>
+          <button class="btn__menu" onClick={openMenu}>
+          <FontAwesomeIcon icon="bars" />
+          
+          </button>
         </ul>
+        <div className="menu__backdrop">
+          <button className="btn__menu btn__menu--close" onClick={closeMenu}>
+            <FontAwesomeIcon icon="times" />
+          </button>
+          
+          <ul className="menu__links">
+            <li className="menu__list">
+              <Link to="/" className="menu__link">
+                Home
+              </Link>
+            </li>
+            <li className="menu__list">
+              <Link to="/movies" className="menu__link">
+                Find Movies
+              </Link>
+            </li>
+            <li className="menu__list">
+              <Link to="/contact" className="menu__link">
+                Contact
+              </Link>
+            </li>
+          </ul></div>
+
       </div>
     </nav>
   );
